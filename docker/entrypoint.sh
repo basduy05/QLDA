1c2e5448fe64693b07a66b4f567d7f61609195cb
NguyenBaDuy <basduygame@gmail.com>
2026-02-16 03:05:15 +0700

    Force Vite manifest in production

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -20,4 +20,6 @@ fi
 mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache
 
+rm -f public/hot
+
 exec apache2-foreground
