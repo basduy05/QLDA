cd0557a536ded93369055c6eaa794073c1f34b99
NguyenBaDuy <basduygame@gmail.com>
2026-02-16 02:24:01 +0700

    Fix storage cache path

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,4 +17,7 @@ if [ "${RUN_MIGRATIONS}" = "true" ]; then
   fi
 fi
 
+mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
+
 exec apache2-foreground
