afe61adb039c056efa3a5644bf4af106567da534
NguyenBaDuy <basduygame@gmail.com>
2026-02-16 17:33:25 +0700

    Fix deploy assets and features

--- app/Notifications/TaskAssignedNotification.php
+++ app/Notifications/TaskAssignedNotification.php
@@ -4,7 +4,6 @@ namespace App\Notifications;
 
 use App\Models\Task;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class TaskAssignedNotification extends Notification
@@ -17,15 +16,7 @@ class TaskAssignedNotification extends Notification
 
     public function via(object $notifiable): array
     {
-        return ['database', 'mail'];
-    }
-
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-            ->subject(__('New task assigned'))
-            ->line(__('You were assigned to task: :title', ['title' => $this->task->title]))
-            ->action(__('Open task'), route('tasks.show', $this->task));
+        return ['database'];
     }
 
     public function toArray(object $notifiable): array
--- app/Notifications/TaskCommented.php
+++ app/Notifications/TaskCommented.php
@@ -4,7 +4,6 @@ namespace App\Notifications;
 
 use App\Models\TaskComment;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class TaskCommented extends Notification
@@ -17,27 +16,7 @@ class TaskCommented extends Notification
 
     public function via(object $notifiable): array
     {
-        return ['database', 'mail'];
-    }
-
-    public function toMail(object $notifiable): MailMessage
-    {
-        $task = $this->comment->task;
-        $project = $task->project;
-        $subject = $this->isMention
-            ? __('You were mentioned in a task comment')
-            : __('New task comment');
-
-        return (new MailMessage())
-            ->subject($subject)
-            ->greeting(__('Hello :name', ['name' => $notifiable->name]))
-            ->line(__(':user commented on task :task', [
-                'user' => $this->comment->user->name,
-                'task' => $task->title,
-            ]))
-            ->line(__(':project project', ['project' => $project->name]))
-            ->line($this->comment->body)
-            ->action(__('View task'), url(route('tasks.show', $task)));
+        return ['database'];
     }
 
     public function toArray(object $notifiable): array
